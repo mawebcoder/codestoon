818bbb22fff1786f3fdc600d9cdc669627c88eb7
codestoon <info@codestoon.com>
2020-12-20 11:16:37 -0700

    end storing articles categorie and testing that

--- app/Http/Controllers/api/v1/admin/article/ArticleCategoryController.php
+++ app/Http/Controllers/api/v1/admin/article/ArticleCategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api\v1\admin\article;
 
 use App\Http\Controllers\Controller;
+use App\models\Article;
 use App\models\ArticleCategory;
 
 class ArticleCategoryController extends Controller
@@ -14,7 +15,20 @@ class ArticleCategoryController extends Controller
 
     public function store()
     {
-
+        $data = request()->only(['fa_title', 'en_title', 'description']);
+        $result = ArticleCategory::create($data);
+        if ($result) {
+            $response_content = [
+                'message' => 'success',
+                'data' => null
+            ];
+            return response()->json($response_content, 201);
+        }
+        $response_content = [
+            'message' => 'failed',
+            'data' => null
+        ];
+        return response()->json($response_content, 500);
     }
 
     public function destroy(ArticleCategory $articleCategory)
@@ -27,7 +41,8 @@ class ArticleCategoryController extends Controller
 
     }
 
-    public function show(ArticleCategory $articleCategory){
+    public function show(ArticleCategory $articleCategory)
+    {
 
     }
 
--- tests/Feature/AdminArticleCategoryTest.php
+++ tests/Feature/AdminArticleCategoryTest.php
@@ -2,21 +2,30 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class AdminArticleCategoryTest extends TestCase
 {
     /**
-     * A basic feature test example.
-     *
-     * @return void
+     * @test
      */
-    public function testExample()
+    public function testCanStoreNewArticleCategory()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $fa_title = Str::random(10);
+        $en_title = Str::random(10);
+        $description = Str::random(100);
+        $data = [
+            'fa_title' => $fa_title,
+            'en_title' => $en_title,
+            'description' => $description
+        ];
+        $this->post(route('categories.store'), $data)
+            ->assertStatus(201)
+            ->assertJson([
+                'message' => 'success',
+                'data' => null
+            ]);
+        $this->assertDatabaseHas('article_categories', $data);
     }
 }
