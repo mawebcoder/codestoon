6a2fb385e7970ed87809f39f5498980cedad55a6
codestoon <info@codestoon.com>
2020-12-21 13:35:43 -0700

    make many to many relationship between articles and article_categories table

--- app/models/Article.php
+++ app/models/Article.php
@@ -3,8 +3,19 @@
 namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Article extends Model
 {
+    use SoftDeletes;
+    protected $guarded = ['id'];
 
+    /**
+     *  make many to many relations between articles table and article_categories table
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function articleCategories()
+    {
+        return $this->belongsToMany(ArticleCategory::class,'article_category','article_id','articleCategory_id');
+    }
 }
--- app/models/ArticleCategory.php
+++ app/models/ArticleCategory.php
@@ -4,8 +4,21 @@ namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+
 class ArticleCategory extends Model
 {
     use SoftDeletes;
-    protected $guarded=['id'];
+
+    protected $guarded = ['id'];
+
+    /**
+     *  make many to many relations between articles table and article_categories table
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function articles()
+    {
+        return $this->belongsToMany(Article::class,'article_category','articleCategory_id','article_id');
+    }
+
 }
