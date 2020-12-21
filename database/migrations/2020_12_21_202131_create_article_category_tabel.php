fc7b6e5d2f966b8416eb62ac43ea07371e1a98ae
codestoon <info@codestoon.com>
2020-12-21 13:28:59 -0700

    add article_category migration to the project

---
+++ database/migrations/2020_12_21_202131_create_article_category_tabel.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleCategoryTabel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_category', function (Blueprint $table) {
+            $table->foreignId('article_id')->references('id')->on('article_categories')->onDelete('cascade');
+            $table->foreignId('articleCategory_id')->references('id')->on('article_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_category');
+    }
+}
---
+++ tests/Feature/AdminArticleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class AdminArticleTest extends TestCase
+{
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testCanStoreNewArticle()
+    {
+
+    }
+}
