71174c135186e7ff857fbbd8b563d8489d91df02
codestoon <info@codestoon.com>
2020-12-23 12:54:11 -0700

    add ArticleTagcontroller to the project and adding store article tag routes to the project

---
+++ app/Http/Controllers/api/v1/admin/article/ArticleTagController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\api\v1\admin\article;
+
+use App\Http\Controllers\Controller;
+
+class ArticleTagController extends Controller
+{
+    public function store()
+    {
+
+    }
+}
--- routes/api/v1/admin/articles/article.php
+++ routes/api/v1/admin/articles/article.php
@@ -8,6 +8,7 @@ Route::post('/articles/categories', 'ArticleCategoryController@store')->name('ar
 Route::put('/articles/categories/{articleCategory}', 'ArticleCategoryController@update')->name('article.category.update');
 Route::delete('/articles/categories/{articleCategory}', 'ArticleCategoryController@destroy')->name('article.category.destroy');
 Route::post('/articles/delete/multi','ArticleController@deleteMultipleArticle')->name('delete.article.multiple');
+Route::post('/articles/tags','ArticleTagController@store')->name('article.tag.store');
 Route::post('/articles/force-delete','ArticleController@forceDeleteMultipleArticle')->name('article.forceDelete');
 Route::resource('articles', 'ArticleController')->except(['create', 'edit','show']);
 
