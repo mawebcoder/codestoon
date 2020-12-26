1206dd65822d66a4079c3c5cbf9cbbb3f4f32fc9
codestoon <info@codestoon.com>
2020-12-26 11:59:44 -0700

    add videos migration to the project

---
+++ database/migrations/2020_12_26_182359_create_videos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('fa_title');
+            $table->string('en_title')->nullable();
+            $table->string('time');
+            $table->boolean('is_free')->default(0);
+            $table->text('description');
+            $table->text('short_description')->nullable();
+            $table->string('meta');
+            $table->string('video_url_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
