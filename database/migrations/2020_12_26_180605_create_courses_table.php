e9511956c30bea7ada5dc867d44b54c019c09cdd
codestoon <info@codestoon.com>
2020-12-26 11:18:48 -0700

    add courses migration to the project

---
+++ database/migrations/2020_12_26_180605_create_courses_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('fa_title');
+            $table->string('en_title')->nullable();
+            $table->string('time')->nullable();
+            $table->decimal('price', 20)->default(0);
+            $table->boolean('has_discount')->default(0);
+            $table->float('discount_value', 10)->default(0);
+            $table->enum('level', ['beginner', 'medium', 'advanced'])->default('beginner');
+            $table->unsignedBigInteger('course_teacher');
+            $table->boolean('is_special_subscription')->default(0);
+            $table->string('course_image_cover')->nullable();
+            $table->text('likes')->nullable();
+            $table->text('description');
+            $table->text('short_description')->nullable();
+            $table->boolean('is_completed_course')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
