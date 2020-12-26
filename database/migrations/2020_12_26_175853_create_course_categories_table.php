7d8de8f28bdd9af1d71ba7bed81e5ecebe60739a
codestoon <info@codestoon.com>
2020-12-26 11:02:56 -0700

    add course category migration to the project

---
+++ database/migrations/2020_12_26_175853_create_course_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('fa_title');
+            $table->string('en_title')->nullable();
+            $table->string('meta')->nullable();
+            $table->longText('description');
+            $table->text('short_description')->nullable();
+            $table->unsignedBigInteger('parent')->default(0);
+            $table->string('course_image_cover_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_categories');
+    }
+}
--- routes/api/v1/admin/courses/course.php
+++ routes/api/v1/admin/courses/course.php
@@ -2,3 +2,4 @@
 use Illuminate\Support\Facades\Route;
 
 
+Route::resource('courses','CourseController');
